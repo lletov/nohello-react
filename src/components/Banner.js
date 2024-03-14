b1b1d4a88db715415ef3d451478cde6fa352cac6
Dmitry <dmitry2595@gmail.com>
2024-03-14 17:38:45 +0300

    feat: banner carcass added

---
+++ src/components/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Banner = () => {
+  return (
+    <div className='banner'>
+        <div className='banner-text'>
+            <h1>no hello</h1>
+            <p className='gray-text'>
+                Пожалуйста, не начинайте разговор в чате с одного «привет». Сообщение без конткеста может заставить собеседника ждать и чувствовать себя немного расстроенным. Вместо этого проявите инициативу и включите свой вопрос или просьбу прямо в первое сообщение! *
+            </p>
+        </div>
+        <div className='banner-content'>
+            <div>
+                Wrong
+            </div>
+            <div className='vertical-line-div'></div>
+            <div>
+                True
+            </div>
+        </div>
+        <h5 className='gray-text'>Узнать больше</h5>
+    </div>
+  )
+}
