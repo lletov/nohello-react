2241010e6c514eb8d2427c69203ae6bd0b34d700
LetovD <dmitry2595@gmail.com>
2024-03-19 10:38:41 +0300

    feat: add message btns icons

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Refresh from './../assets/refresh.png'
+import Copy from './../assets/copy.png'
 
 export const Message = () => {
   return (
@@ -9,8 +11,14 @@ export const Message = () => {
         <div className='message'>
             <div>Text</div>
             <div className='message-btns'>
-                <a href='#'>Другой вариант</a>
-                <a href='#'>Скопировать</a>
+              <div className='message-btn'>
+                <img src={Refresh}/>
+                <p>Другой вариант</p>
+              </div>
+              <div className='message-btn'>
+                <img src={Copy}/>
+                <p>Скопировать</p>
+              </div>
             </div>
         </div>
     </div>
