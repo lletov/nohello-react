77436537490b80b27ebef73aec96e9ccce54725e
LetovD <dmitry2595@gmail.com>
2024-03-19 12:42:40 +0300

    fix: smth

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Loader = () => {
   return (
     <div className='loader'>
-        <span class="spinner"></span>
+        <span className="spinner"></span>
         <p>Генерируем... Это займет несколько секунд</p>
     </div>
   )
