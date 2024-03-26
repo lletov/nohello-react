08ab71bb4c7bfa1ecb6623c5c4a5f9d06c0d0e5c
LetovD <dmitry2595@gmail.com>
2024-03-26 11:33:51 +0300

    feat: select & some fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,8 +11,8 @@ export const Footer = () => {
         <div className='footer-content'>
             <div className='footer-content-row'>
                 <p>Copyright © 2024</p>
-                <a href=''>О проекте</a>
-                <a href=''>Страница проекта на GitHub</a>
+                {/* <a href=''>О проекте</a> */}
+                <a href='https://github.com/lletov/nohello-react' target='_blank'>Страница проекта на GitHub</a>
             </div>
             <div>
                 Язык: Русский
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import DownArrow from './../assets/arrow_down.png'
 
 export const Header = () => {
   return (
     <div className='header'>
         <div className='header-content'>
-            <div className='lang-change-btn'>
-            <h5>Русский</h5>
-            <img src={DownArrow}/>
-            </div>
+            <select className='lang-change-btn'>
+            <option>Русский</option>
+            </select>
         </div>
     </div>
   )
