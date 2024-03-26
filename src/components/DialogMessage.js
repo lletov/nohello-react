523ecbf3584b05539ce539318b3de93627e359e7
LetovD <dmitry2595@gmail.com>
2024-03-26 11:02:34 +0300

    feat: responsive

--- src/components/DialogMessage.js
+++ src/components/DialogMessage.js
@@ -2,29 +2,58 @@ import React from 'react'
 import { cssVars } from '../CssVars'
 
 export const DialogMessage = ({user, tone, text}) => {
-    if (user == 'left') {
-        return (
-            <div 
-                className='dialog-message'
-                style={{backgroundColor: tone =='true' ? cssVars.light_green : cssVars.light_orange, 
-                color: tone =='true'? cssVars.green : cssVars.orange,
-                textAlign: 'left',
-                marginRight: '120px'}}
-            >
+    if (window.screen.width > 480){
+        if (user === 'left') {
+            return (
+                <div 
+                    className='dialog-message'
+                    style={{backgroundColor: tone ==='true' ? cssVars.light_green : cssVars.light_orange, 
+                    color: tone ==='true'? cssVars.green : cssVars.orange,
+                    textAlign: 'left',
+                    marginRight: '120px'}}
+                >
+                    <p>{text}</p>
+                </div>
+            )
+        } else {
+            return (
+                <div 
+                    className='dialog-message'
+                    style={{backgroundColor: cssVars.light_gray, 
+                    color: cssVars.gray,
+                    textAlign: 'right',
+                    marginLeft: '120px'}}
+                >
                 <p>{text}</p>
             </div>
-        )
+            )
+        }
     } else {
-        return (
-            <div 
-                className='dialog-message'
-                style={{backgroundColor: cssVars.light_gray, 
-                color: cssVars.gray,
-                textAlign: 'right',
-                marginLeft: '120px'}}
-            >
-            <p>{text}</p>
-        </div>
-        )
+        if (user === 'left') {
+            return (
+                <div 
+                    className='dialog-message'
+                    style={{backgroundColor: tone ==='true' ? cssVars.light_green : cssVars.light_orange, 
+                    color: tone ==='true'? cssVars.green : cssVars.orange,
+                    textAlign: 'left',
+                    marginRight: '60px'}}
+                >
+                    <p>{text}</p>
+                </div>
+            )
+        } else {
+            return (
+                <div 
+                    className='dialog-message'
+                    style={{backgroundColor: cssVars.light_gray, 
+                    color: cssVars.gray,
+                    textAlign: 'right',
+                    marginLeft: '60px'}}
+                >
+                <p>{text}</p>
+            </div>
+            )
+        }
     }
+    
 }
