673cf2e90f69987be5a3db3b5e647f534f3604ed
Dmitry <dmitry2595@gmail.com>
2024-03-29 13:33:23 +0300

    feat: style fix

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -65,7 +65,7 @@ export const Banner = () => {
                 
             </div>
         </div>
-        <a href='#reasons'><h6 className='gray-text'>Узнать больше</h6></a>
+        <a href='#reasons'><h6 className='gray-text' id='find-more-link'>Узнать больше</h6></a>
     </div>
   )
 }
