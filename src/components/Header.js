d081a17a1afa7777cb4bac85166199798ba48f0d
LetovD <dmitry2595@gmail.com>
2024-04-01 14:41:34 +0300

    feat: add lang file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { LANGUAGES } from "./../constants/index.ts";
 
 export const Header = () => {
   return (
     <div className='header'>
         <div className='header-content'>
-            <select className='lang-change-btn'>
-            <option>Русский</option>
-            </select>
+        <select defaultValue={"es"}>
+          {LANGUAGES.map(({ code, label }) => (
+            <option key={code} value={code}>
+              {label}
+            </option>
+          ))}
+        </select>
         </div>
     </div>
   )
---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const LANGUAGES =[
+    {label: 'Русский', code: 'ru'},
+    {label: 'English', code: 'en'}
+];
